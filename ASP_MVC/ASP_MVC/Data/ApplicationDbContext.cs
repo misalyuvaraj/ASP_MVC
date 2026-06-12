8cc76070a2a75d27a4161a56003e23194ee81c60
Yuvaraj Misal <misalyuvi@gmail.com>
2026-06-12 16:01:10 +0530

    Complet section 1 and  2  Working done and  Add Category modela and connection in SQL

---
+++ ASP_MVC/ASP_MVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using ASP_MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP_MVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions <ApplicationDbContext> options) : base(options) 
+        {
+
+        }
+     
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().HasData(
+
+                new Category { Id = 1, Name="Action"},
+                new Category { Id = 2, Name="Scrifi" },
+                new Category { Id = 3 , Name = "History"}
+                );
+        }
+    }
+}
